d359f7f3047d1e23dc4699b3f0c3f69fe962c4d7
johanBautista <jsbparra@gmail.com>
2023-12-17 01:01:12 +0100

    add function to get to file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,121 +1,112 @@
 <template>
-  <div class="hello">
+  <div class="bg-gray-500">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <input type="file" @change="onChange" />
+
+    <!-- 
+    <div>
+      <div v-for="item in conceptoConGastos" :key="item.concepto">
+        <b>{{ item.concepto }} = {{ item.totalGastos }}</b>
+      </div>
+    </div>
+     -->
+    <div v-if="conceptoConGastos.length > 0">
+      <table>
+        <thead>
+          <tr>
+            <th>Concepto</th>
+            <th>Importe</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in conceptoConGastos" :key="item.concepto">
+            <td>{{ item.concepto }}</td>
+            <td>{{ item.totalGastos }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import { read, utils } from "xlsx";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  components: {},
+  data() {
+    return {
+      file: [],
+    };
+  },
+  computed: {
+    conceptoConGastos() {
+      const conceptos = {};
+      if (!this.file) {
+        return {};
+      }
+
+      // Procesar los datos para obtener la información reducida por concepto
+      this.file.forEach((row) => {
+        const [fecha, concepto, importe] = row;
+
+        if (!conceptos[concepto]) {
+          conceptos[concepto] = { concepto, gastos: [], totalGastos: 0 };
+        }
+
+        const importeNum = Number(importe);
+        conceptos[concepto].gastos.push({ fecha, importe: importeNum });
+        conceptos[concepto].totalGastos = conceptos[concepto].gastos.reduce(
+          (sum, item) => sum + item.importe,
+          0
+        );
+      });
+
+      // Convertir el objeto de conceptos en un array
+      console.log("conceptos", conceptos);
+      return Object.values(conceptos);
+    },
+  },
+
+  methods: {
+    onChange(event) {
+      if (event.target.files && event.target.files.length > 0) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const wb = read(e.target.result, { type: "binary" });
+          const wsname = wb.SheetNames[0];
+          const ws = wb.Sheets[wsname];
+          const data = utils.sheet_to_json(ws, { header: 1 });
+          console.log("DATA", data);
+          this.file = data;
+        };
+
+        reader.readAsBinaryString(event.target.files[0]);
+      }
+    },
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
